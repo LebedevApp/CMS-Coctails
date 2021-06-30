5b1928b30c6eee747fa44be433a065e74a84da45
Dmitryy <llle8evvv@yandex.ru>
2021-06-30 18:47:16 +0300

    server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path')
 
 app = express()
 
-app.use(serveStatic(__dirname))
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
 const port = process.env.PORT || 5000
 
