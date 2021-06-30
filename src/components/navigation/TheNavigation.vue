c7814c94cd8c2f3363a14338e12785e9dec5ee13
Dmitryy <llle8evvv@yandex.ru>
2021-06-30 19:01:48 +0300

    first commit

--- src/components/navigation/TheNavigation.vue
+++ src/components/navigation/TheNavigation.vue
@@ -3,9 +3,20 @@
       <v-divider></v-divider>
     <v-button-link :item="menu" />
 
-    <div class="links mt-10">
+    <div class="links mt-12">
       <v-divider></v-divider>
       <v-button-link v-for="(item, i) in links" :key="i" :item="item" />
+    </div>
+
+    <div class="mt-12">
+      <v-divider></v-divider>
+      <v-list-item link :href="github.to" color="light-green accent-3">
+        <v-icon class="mr-3">{{github.icon}}</v-icon>
+        <v-list-item-content>
+          <v-list-item-title class="text-subtitle-2 font-weight-light">{{github.name}}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
     </div>
 
     <v-button-link class="logout" :item="logout" />
@@ -35,6 +46,11 @@ export default {
         name: "Настройки",
         icon: "mdi-menu",
         to: "/options",
+      },
+      github: {
+        name: "GitHub",
+        icon: "mdi-github ",
+        to: "https://github.com/Podsida/CMS-Coctails.git",
       },
     };
   },
