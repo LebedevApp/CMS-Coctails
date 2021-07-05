49b6683c59d0c18b5999a4b91a1ab8252425dbc2
Dmitryy <llle8evvv@yandex.ru>
2021-07-05 13:03:10 +0300

    add connect-history-api-fallback

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
 
+const history = require('connect-history-api-fallback');
+
 app = express()
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+app.use(history());
 
 const port = process.env.PORT || 5000
 
